d346adedf0ee5684cd9f529e5cf9588c2283f3f2
ahmedemad3 <ahmedemad3@gmail.com>
2021-08-29 04:38:49 +0200

    commit Eureka server

---
+++ eu-service-registry/src/test/java/eg/com/eu/EuServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.com.eu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
